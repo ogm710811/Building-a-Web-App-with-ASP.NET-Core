79f338de74eca4ef052ebcb56a35dfaf5ae68758
ogm710811 <orestes.garcia.martinez@gmail.com>
2018-08-07 00:30:05 -0400

    Using ASP.NET MVC - Using Bootstrap to Prototype the App - Building the Database with Entity Framework Core

---
+++ DutchTreat/Data/DutchSeeder.cs
@@ -0,0 +1,59 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class DutchSeeder
+    {
+        private readonly DutchContext _ctx;
+        private readonly IHostingEnvironment _hostingEnviroment;
+
+        public DutchSeeder(DutchContext ctx, IHostingEnvironment hostingEnviroment)
+        {
+            this._ctx = ctx;
+            this._hostingEnviroment = hostingEnviroment;
+        }
+
+        public void Seed()
+        {
+            _ctx.Database.EnsureCreated();
+            if (!_ctx.Products.Any())
+            {
+                // need to create sample data
+                var filePath = Path.Combine(_hostingEnviroment.ContentRootPath, "Data/art.json");
+                var jasonData = File.ReadAllText(filePath);
+                var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(jasonData);
+
+                // adding products to the DB
+                _ctx.Products.AddRange(products);
+
+                var order = new Order()
+                {
+                    OrderDate = DateTime.Now,
+                    OrderNumber = "12345",
+                    Items = new List<OrderItem>()
+                    {
+                        new OrderItem()
+                        {
+                            Product = products.First(),
+                            Quantity = 5,
+                            UnitPrice = products.First().Price
+                        }
+                    }
+
+                };
+
+                // adding order to the DB
+                _ctx.Orders.Add(order);
+
+                _ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DutchTreat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
+                .UseStartup<Startup>();
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            //  removing the default configuration options
+            builder.Sources.Clear();
+
+            // create config file
+            builder.AddJsonFile("config.json", false, true)
+                   .AddXmlFile("config.xml", true)
+                   .AddEnvironmentVariables();
+
+        }
+    }
+}
